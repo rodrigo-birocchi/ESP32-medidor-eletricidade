b525781573a7e8588bcef69e3b09b4abb5038d3c
Rodrigo Birocchi <126339547+rodrigo-birocchi@users.noreply.github.com>
2023-11-17 16:15:22 -0300

    Adicionada ferramenta gráfica à página inicial e removidos arquivos não utilizados

--- www/script.js
+++ www/script.js
@@ -1,14 +1,58 @@
-async function getValue(){
+// Updates the data with the latest value and shift data set forward 
+async function update(data, i){
+
     const numero = fetch('http://' + window.location.hostname + '/value')
-        .then(response => response.json())
-        .then(data => { updateNumber(data.value) });
+    .then(response => response.json())
+    .then(convertido => {
+        
+        data.append({
+            x: i,
+            value: convertido.value
+        });
+
+        data.remove(0);
+    });
 }
 
-function updateNumber(n) {
-    const div = document.getElementById("result");
-    div.innerHTML = n;
-}
+anychart.onDocumentReady(function () {
 
-console.log("Funcionando");
+    // create list to store the last 20s of data
+    var data = [
+        ["20", 0],["19", 0],["18", 0],["17", 0],
+        ["16", 0],["15", 0],["14", 0],["13", 0],
+        ["12", 0],["11", 0],["10", 0],["9", 0],
+        ["8", 0],["7", 0],["6", 0],["5", 0],
+        ["4", 0],["3", 0],["2", 0],["1", 0]
+    ];
 
-window.setInterval(getValue, 250);
+    // create a data set
+    var dataSet = anychart.data.set(data);
+
+    // map the data for series
+    var seriesData = dataSet.mapAs({x: 0, value: 1});
+
+    // create a line chart
+    var chart = anychart.line();
+
+    // create the series and name it
+    var series = chart.line(seriesData);
+    series.name("ACS712");
+
+    // add a legend
+    chart.legend().enabled(true);
+
+    // add a title
+    chart.title("Leitura ACS712");
+
+    // specify where to display the chart
+    chart.container("container");
+
+    // draw the resulting chart
+    chart.draw();
+
+    var indexSetter = 0;
+    window.setInterval(function () {
+        update(dataSet, indexSetter);
+        indexSetter++;
+    }, 1000);
+});
