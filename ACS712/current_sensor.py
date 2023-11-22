3784c123b9788017c5bf256ec65f1b5d8590f539
Rodrigo Birocchi <126339547+rodrigo-birocchi@users.noreply.github.com>
2023-11-22 16:44:12 -0300

    Alteração da função de calibração do sensor

--- ACS712/current_sensor.py
+++ ACS712/current_sensor.py
@@ -4,15 +4,15 @@ from time import sleep
 
 # Encontra o zero do sensor
 def auto_zero():
-    print("Fazendo ao auto zero do sensor...")
-    menor_valor = 4095
+    # print("Fazendo ao auto zero do sensor...")
+    # menor_valor = 4095
 
-    for i in range(1, 10000):
-        valor_atual = pontentiometer.read()
-        if valor_atual < menor_valor: menor_valor = valor_atual
-        sleep(1/1000000) # espere 1 microsegundo
+    # for i in range(1, 10000):
+    #     valor_atual = pontentiometer.read()
+    #     if valor_atual < menor_valor: menor_valor = valor_atual
+    #     sleep(1/1000000) # espere 1 microsegundo
 
-    return menor_valor
+    return 2900
 
 # Encontra o menor valor no sensor
 def menor_valor():
@@ -21,7 +21,7 @@ def menor_valor():
     for i in range(1, 1600):
         valor_atual = pontentiometer.read()
         if valor_atual < menor_valor: menor_valor = valor_atual
-        sleep(1/100000) # espere 10 microsegundos
+        sleep(1/10000)
 
     return menor_valor
 
@@ -31,13 +31,6 @@ def valor():
     volts_value = pontentiometer_value * 0.805 # amplitude total / resolucao da maquina = valor em volts
     final_value = volts_value / 185 # de acordo com a datasheet do ACS712-05B
     return final_value
-
-# Mostra resultados no console com formatação
-def print_result(res):
-    print("Corrente de pico:")
-    print(str(res) + "A")
-    print("-----------------")
-    print(str(res * 1000) + "mA")
     
 
 # Inicia comunicação analogica com o sensor usando o pino 32
